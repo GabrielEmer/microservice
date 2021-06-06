a625551ae66cdbd6a271f738c505930d3e8726c7
Gabriel Emer <gabriel_emer@hotmail.com>
2021-06-06 10:20:19 -0300

    Project Eureka Server created

---
+++ hr-eureka-server/src/test/java/com/gabrielemer/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielemer.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
