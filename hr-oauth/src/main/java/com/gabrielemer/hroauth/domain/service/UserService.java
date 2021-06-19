185f09ba5b837f0e612a920598a3f143ea41de4e
Gabriel Emer <gabriel_emer@hotmail.com>
2021-06-19 18:15:16 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/gabrielemer/hroauth/domain/service/UserService.java
+++ hr-oauth/src/main/java/com/gabrielemer/hroauth/domain/service/UserService.java
@@ -3,12 +3,15 @@ package com.gabrielemer.hroauth.domain.service;
 import com.gabrielemer.hroauth.domain.model.User;
 import com.gabrielemer.hroauth.feign.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
   @Autowired
   private UserFeignClient userFeignClient;
@@ -17,6 +20,15 @@ public class UserService {
     User user = userFeignClient.findByEmail(email).getBody();
     if (user == null) {
       throw new IllegalArgumentException("Email not found");
+    }
+    return user;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userFeignClient.findByEmail(username).getBody();
+    if (user == null) {
+      throw new UsernameNotFoundException("Email not found");
     }
     return user;
   }
